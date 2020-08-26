b051e505ddfb5d8080bee78c95c6eb16f9c61ddb
lorelei <36687196+liyihangaaa@users.noreply.github.com>
2020-08-26 22:28:42 +0800

    Add files via upload

--- application.R
+++ application.R
@@ -12,11 +12,12 @@ M3 <-
     b1<-x[,1]
     b2<-x[,2]
     
-    c1 <- 1-pnorm(0.05, mean =0.0394, sd= 0.0587, lower.tail = TRUE, log.p = FALSE)
-    c2 <- 1-pnorm(0.05, mean =0.0456, sd= 0.16389, lower.tail = TRUE, log.p = FALSE)
-    c3 <- 1-pnorm(0.05, mean =0.0875, sd= 0.23871, lower.tail = TRUE, log.p = FALSE)
-    return(cbind(-b1*pow(1.0394,0.9)/0.9 - b2*pow(1.0456,0.75)/0.75 - (1-b1-b2)*pow(1.0875,0.65)/0.65,
-                 b1*exp(0.172+c1)+b2*exp(0.112+c2)+(1-b1-b2)*exp(0.182+c3)
+    
+    c1 <- 1-pnorm(0.05, mean =0.394, sd= 0.241, lower.tail = TRUE, log.p = FALSE)
+    c2 <- 1-pnorm(0.05, mean =0.456, sd= 0.403, lower.tail = TRUE, log.p = FALSE)
+    c3 <- 1-pnorm(0.05, mean =0.875, sd= 0.488, lower.tail = TRUE, log.p = FALSE)
+    return(cbind(-b1*pow(1.394,0.9)/0.9 - b2*pow(1.456,0.75)/0.75 - (1-b1-b2)*pow(1.875,0.65)/0.65,
+                 b1*(1.62+c1)+b2*(0.92+c2)+(1-b1-b2)*(1.67+c3)
     ) 
     )
   }
@@ -26,7 +27,7 @@ n_var <- 2
 fname <- M3
 lower <- rep(0, n_var)
 upper <- rep(0.5, n_var)
-res <- easyGParetoptim(fn=fname, lower=lower, upper=upper, budget=20,
+res <- easyGParetoptim(fn=fname, lower=lower, upper=upper, budget=40,
                        control=list(method="EHI", inneroptim="pso", maxit=20))
 par(mfrow=c(1,2))
 plotGPareto(res)
@@ -56,5 +57,3 @@ lines(dat[,3], ann = F,col="blue",lty=1)
 
 #legend("topright",cex=0.4,c("Gold","NASDAQ","Apple"),col=c("black","red","blue"),lty=1)
 legend("topright", cex=0.2,inset=.01, c("Gold","NASDAQ","Apple"),lty=1, col=c("black","red","blue"))
-
-
